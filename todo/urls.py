2b110f9eaf09f14bd033ec5689be3d14f62f504a
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-09 15:48:57 +0900

    csrf off when debug

--- todo/urls.py
+++ todo/urls.py
@@ -24,6 +24,19 @@ urlpatterns = [
     path('graphql/', GraphQLView.as_view(graphiql=settings.DEBUG)),
 ]
 
+
+if settings.DEBUG:
+    from django.views.decorators.csrf import csrf_exempt
+    urlpatterns += [
+        path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
+    ]
+else:
+    # TODO: どうするのがいいのか・・・
+    urlpatterns += [
+        path('graphql/', GraphQLView.as_view(graphiql=settings.DEBUG)),
+    ]
+
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
