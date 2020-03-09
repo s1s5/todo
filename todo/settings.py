07e611963015452fc1c09bf77b7d21a5f35a052b
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-09 15:56:57 +0900

    bug fix

--- todo/settings.py
+++ todo/settings.py
@@ -237,12 +237,12 @@ MIDDLEWARE.insert(
 CORS_ORIGIN_REGEX_WHITELIST = [
     r"^http://localhost:.*$",
 ]
-# CORS_ORIGIN_WHITELIST = [
-#     "https://example.com",
-#     "https://sub.example.com",
-#     "http://localhost:8080",
-#     "http://127.0.0.1:9000"
-# ]
+#CORS_ORIGIN_WHITELIST = [
+#    "https://example.com",
+#    "https://sub.example.com",
+#    "http://localhost:8080",
+#    "http://127.0.0.1:42100",
+#]
 
 
 ############################################
--- todo/urls.py
+++ todo/urls.py
@@ -21,7 +21,6 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=settings.DEBUG)),
 ]
 
 
