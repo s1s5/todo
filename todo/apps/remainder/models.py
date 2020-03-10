56860087f5a1115bed298883167ab8c2409012fb
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-10 23:06:27 +0900

    add created_at, modified_at

--- todo/apps/remainder/models.py
+++ todo/apps/remainder/models.py
@@ -6,8 +6,20 @@ class TodoList(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at', ]
+
 
 class Todo(models.Model):
     parent = models.ForeignKey(TodoList, on_delete=models.PROTECT)
     completed = models.BooleanField()
     text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at', ]
