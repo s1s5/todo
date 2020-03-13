22d2953a4e54e4757fea6cbfcae782b13b39bd34
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-13 15:36:51 +0900

    fix leak

--- todo/routing.py
+++ todo/routing.py
@@ -1,3 +1,5 @@
+import functools
+import json
 import logging
 
 from channels.auth import AuthMiddlewareStack
@@ -5,21 +7,70 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from django.conf import settings
 
-from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
-
+from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer, AttrDict
+from graphene_django.settings import graphene_settings
 
 logger = logging.getLogger(__name__)
 
 
 class CustomGraphqlSubscriptionConsumer(GraphqlSubscriptionConsumer):
-    def _send_result(self, id, result):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.disposable_list = []
+
+    def websocket_connect(self, message):
+        logger.debug('websocket_connect(%s): %s', id(self), message)
+        super().websocket_connect(message)
+
+    def websocket_disconnect(self, message):
+        logger.debug('websocket_disconnect(%s): %s', id(self), message)
+        for disposable in self.disposable_list:
+            disposable.dispose()
+        self.disposable_list = []
+        logger.debug('websocket_disconnect(%s): clear all disposable', id(self))
+        super().websocket_disconnect(message)
+
+    def websocket_receive(self, message):
+        logger.debug('websocket_receive(%s): %s', id(self), message)
+
+        request = json.loads(message["text"])
+        _id = request.get("id")
+
+        if request["type"] == "connection_init":
+            return
+
+        elif request["type"] == "start":
+            payload = request["payload"]
+            context = AttrDict(self.scope)
+
+            schema = graphene_settings.SCHEMA
+
+            result = schema.execute(
+                payload["query"],
+                operation_name=payload.get("operationName"),
+                variables=payload.get("variables"),
+                context=context,
+                root=stream,
+                allow_subscriptions=True,
+            )
+
+            if hasattr(result, "subscribe"):
+                disposable = result.subscribe(functools.partial(self._send_result, _id))
+                self.disposable_list.append(disposable)
+            else:
+                self._send_result(_id, result)
+
+        elif request["type"] == "stop":
+            pass
+
+    def _send_result(self, _id, result):
         if result.errors:
             for error in result.errors:
                 logger.error('subscription error',
                              exc_info=(type(error), error,
                                        error.__traceback__))
-        logger.debug('sending result %s', result.data)
-        return super()._send_result(id, result)
+        logger.debug('sending result(%s) %s', id(self), result.data)
+        return super()._send_result(_id, result)
 
 
 if settings.DEBUG:
