2492df83727615638192217fe941f9d27c441eb0
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-13 11:56:24 +0900

    add memo

--- todo/apps/remainder/schema.py
+++ todo/apps/remainder/schema.py
@@ -39,20 +39,15 @@ class TodoFilterSet(FilterSet):
         )
     )
 
-    # TODO
-    # def filter_queryset(self, queryset):
-    #     """
-    #     Filter the queryset with the underlying form's `cleaned_data`. You must
-    #     call `is_valid()` or `errors` before calling this method.
-    #     This method should be overridden if additional filtering needs to be
-    #     applied to the queryset before it is cached.
-    #     """
-    #     for name, value in self.form.cleaned_data.items():
-    #         queryset = self.filters[name].filter(queryset, value)
-    #         assert isinstance(queryset, models.QuerySet), \
-    #             "Expected '%s.%s' to return a QuerySet, but got a %s instead." \
-    #             % (type(self).__name__, name, type(queryset).__name__)
-    #     return queryset
+    def filter_queryset(self, queryset):
+        """
+        Filter the queryset with the underlying form s `cleaned_data`. You must
+        call `is_valid()` or `errors` before calling this method.
+        This method should be overridden if additional filtering needs to be
+        applied to the queryset before it is cached.
+        """
+        # use self.request.user or something
+        return super().filter_queryset(queryset)
 
 
 class TodoNode(DjangoObjectType):
