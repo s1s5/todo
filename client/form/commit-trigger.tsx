413b48c450b3ea153cdb2cc64a3c3a1c8a8968aa
Shogo Sawai <shogo.sawai@gmail.com>
2020-04-05 15:33:13 +0900

    support react node

--- client/form/commit-trigger.tsx
+++ client/form/commit-trigger.tsx
@@ -5,14 +5,18 @@ import FormContext from './form-context'
 type Props<T> = {
     onSuccess?: (response: T) => unknown,
     onFailure?: (response: T) => unknown,
-    children: (commit: () => void) => JSX.Element
+    children: ((commit: () => void) => JSX.Element) | React.ReactNode
 }
 
 const CommitTrigger = <T extends Object>(props: Props<T>) => {
     return <FormContext.Consumer>
-      { (context) => (
-          props.children(() => context!.commit(props.onSuccess, props.onFailure))
-      )}
+      { (context) => {
+          if (props.children instanceof Function) {
+              return props.children((on_success?: (response: T) => unknown, on_failure?: (response: T) => unknown) => context!.commit(on_success ? on_success : props.onSuccess, on_failure ? on_failure : props.onFailure))
+          } else {
+              return React.cloneElement(props.children as any, { onClick: () => context!.commit(props.onSuccess, props.onFailure) })  // TODO: type check
+          }
+      }}
     </FormContext.Consumer>
 }
 
